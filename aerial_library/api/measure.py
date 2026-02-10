70b22fb8d56d432db057caa87c5e6f044d09bbac
Konstantin Lindorfer <konstantin.lindorfer@spyteam.one>
2026-02-10 08:43:07 +0100

    Add impl for measure API (to be tested)

--- aerial_library/api/measure.py
+++ aerial_library/api/measure.py
@@ -1,7 +1,26 @@
-from aerial_library.backend.multirangerdeck import MultiRangerDeck
+from math import inf as infinity
 
+from aerial_library.backend.multirangerdeck import MultiRangerDeck
 
 
 class Measure:
     def __init__(self, backend: MultiRangerDeck):
         self._backend = backend
+
+    def front(self) -> float:
+        return self._backend.get_front_distance_m() or infinity
+
+    def back(self) -> float:
+        return self._backend.get_back_distance_m() or infinity
+
+    def left(self) -> float:
+        return self._backend.get_left_distance_m() or infinity
+
+    def right(self) -> float:
+        return self._backend.get_right_distance_m() or infinity
+
+    def up(self) -> float:
+        return self._backend.get_up_distance_m() or infinity
+
+    def down(self) -> float:
+        return self._backend.get_down_distance_m() or infinity
--- aerial_library/backend/multirangerdeck.py
+++ aerial_library/backend/multirangerdeck.py
@@ -1,4 +1,6 @@
-from typing import ContextManager, TYPE_CHECKING
+from typing import ContextManager, TYPE_CHECKING, Optional
+
+from cflib.utils.multiranger import Multiranger
 
 from aerial_library.api.errors import MultiRangerDeckNotFound
 
@@ -9,10 +11,31 @@ if TYPE_CHECKING:
 class MultiRangerDeck(ContextManager):
     def __init__(self, drone: "Drone"):
         self._drone = drone
+        self._deck = Multiranger(crazyflie=drone.cf, rate_ms=10)
 
     def __enter__(self):
         if not self._drone.has_deck("bcMultiranger"):
             raise MultiRangerDeckNotFound()
 
-    def __exit__(self, exc_type, exc_value, traceback, /):
-        pass
+        self._deck.start()
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self._deck.stop()
+
+    def get_front_distance_m(self) -> Optional[float]:
+        return self._deck.front
+
+    def get_back_distance_m(self) -> Optional[float]:
+        return self._deck.back
+
+    def get_left_distance_m(self) -> Optional[float]:
+        return self._deck.left
+
+    def get_right_distance_m(self) -> Optional[float]:
+        return self._deck.right
+
+    def get_up_distance_m(self) -> Optional[float]:
+        return self._deck.up
+
+    def get_down_distance_m(self) -> Optional[float]:
+        return self._deck.down
