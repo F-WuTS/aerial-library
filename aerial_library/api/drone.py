f7ba2d7654d1ddda280a2a829d710740de90fb0d
Konstantin Lindorfer <konstantin.lindorfer@spyteam.one>
2026-02-12 19:51:55 +0100

    Adapt API documentation for Drone

--- aerial_library/api/drone.py
+++ aerial_library/api/drone.py
@@ -9,37 +9,48 @@ from aerial_library.backend.util import select_connection
 
 
 class Drone(ContextManager):
-    """ "Drone" is your entrypoint to programming your Crazyflie drone, intended to be used in a with-statement.
+    """ `Drone` is your entrypoint to programming your Crazyflie drone.
 
-    This class will take care of almost everything.
-    The user can focus on the actual logic code without distractions:
+    Instantiate a `Drone` as a *context manager* in a `with`-statement.
+    Declare the `Features` you would like to use in its constructor, for example `FlowDeck`.
+    The returned object should be named and is then used to control the drone:
 
-    When running the with-block, the user is first asked which drone to connect to.
-    If only one drone is found, that drone will be used automatically.
-    If no drones are found, an error will be raised.
+    >>> from aerial_library import Drone, FlowDeck
+    >>>
+    >>> with Drone(FlowDeck) as minimal_drone:
+    ...     minimal_drone.move.takeoff(1.0)
 
-    Once connected, the code inside the with-block is run.
-    The ``move`` and ``measure`` APIs can be used here to control the drone.
+    This *context manager* will take care of almost everything:
 
-    At the end of the with-block, the drone is automatically landed if necessary.
-    This is also the case should the program raise an error while the drone is flying.
-    After landing, the Crazyflie is disconnected.
+    * When running the `with`-block, the user is first asked which drone to connect to.
+      If only one drone is found, that drone will be used automatically.
+      If no drones are found, an error will be raised.
+
+    * Once connected, the additional features are loaded and the drone is checked for the required hardware.
+      The battery state is displayed in the terminal for manual checking.
+
+    * Now the code inside the `with`-block is executed.
+      Use the ``move`` and ``measure`` APIs on the Actions object returned by the `with`-statement to control the drone.
+
+    * At the end of the `with`-block, the drone is automatically landed if necessary.
+      This is also the case should the program raise an error while the drone is flying.
+      After landing, the Crazyflie is disconnected.
 
     Example:
-        Here is a short example script that connects to a Crazyflie with all features enabled.
+        This short script connects to a Crazyflie with all features enabled.
         The drone takes off at an altitude of 1 meter and measures the distance to the front:
 
         >>> from aerial_library import Drone, FlowDeck, MultiRangerDeck, FastMode
         >>>
         >>> with Drone(
-        ...    FlowDeck,
-        ...    MultiRangerDeck,
-        ...    FastMode,
+        ...     FlowDeck,
+        ...     MultiRangerDeck,
+        ...     FastMode,
         ... ) as drone:
-        ...    drone.move.takeoff(1.0)
+        ...     drone.move.takeoff(1.0)
         ...
-        ...    distance = drone.measure.front()
-        ...    print(f"Distance to front in meters: {distance}")
+        ...     distance = drone.measure.front()
+        ...     print(f"Distance to front in meters: {distance}")
 
     """
 
