4ffb0fb6415ccf13b42c51f27d8ac5a8f10fc0f8
Konstantin Lindorfer <konstantin.lindorfer@spyteam.one>
2026-02-19 09:36:00 +0100

    Add module-level documentation; Expose Actions class for automatic documentation generation

--- aerial_library/__init__.py
+++ aerial_library/__init__.py
@@ -1,8 +1,17 @@
+"""
+This library provides simple and intuitive controls for a Crazyflie drone.
+
+To get started, please see the user guide in the downloads section of our website:
+https://aerial-challenge.org/downloads/
+"""
+
 from aerial_library.api.drone import Drone
+from aerial_library.api.actions import Actions
 from aerial_library.api.feature import FlowDeck, MultiRangerDeck
 
 __all__ = [
     "Drone",
+    "Actions",
     "FlowDeck",
     "MultiRangerDeck",
 ]
